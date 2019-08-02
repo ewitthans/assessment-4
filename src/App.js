ca8174c53e5f1f85db7e33604250f3ae068aec89
Eric Witthans <homesteadlaptop@Erics-MBP.attlocal.net>
2019-08-01 19:38:22 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import './App.css';
+import { Navbar, Form, FormControl, Button } from 'react-bootstrap';
+import { Route, NavLink } from 'react-router-dom';
+import MainComponent from './component/MainComponent';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCocktail } from '@fortawesome/free-solid-svg-icons'
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+  }
+  render() {
+    return (
+      <div className='Container'>
+        <Navbar bg="light" expand="lg">
+          <div>
+            <FontAwesomeIcon icon={faCocktail} />
+          </div>
+          <Navbar.Brand href="#">Mix It Up!</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Form inline>
+              <FormControl type="text" placeholder="Search" id="Liquor"/>
+              <Button variant="outline-success" onClick={() => this.initateSearch()}>Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+        <div className='Wrapper'>
+          {/* how to add homepage, route, components */}
+          <Route
+            path="/" exact
+            render={(props) => <MainComponent />}
+          />
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import MainComponent from './component/MainComponent'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+
+    <Router>
+        <Route
+            path="/"
+            component={App}
+        />
+        <Route
+            path="/main"
+            component={MainComponent}
+        />
+    </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
